9e3e5c46e1c3724b2b1a5dadd101e882807f3f45
wenqiurong <wenqiurong@jinlinbao.com>
2016-06-22 17:16:29 +0800

    covert to studio project and adjust ndk config

--- ZBarBuild/obj/local/arm64-v8a/objs/ZBarDecoder/zbar/qrcode/bch15_5.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/arm64-v8a/objs/ZBarDecoder/zbar/qrcode/bch15_5.o: \
- jni/zbar/qrcode/bch15_5.c jni/zbar/qrcode/bch15_5.h
-
-jni/zbar/qrcode/bch15_5.h:
--- ZBarBuild/obj/local/x86/objs/ZBarDecoder/zbar/qrcode/bch15_5.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/x86/objs/ZBarDecoder/zbar/qrcode/bch15_5.o: \
- jni/zbar/qrcode/bch15_5.c jni/zbar/qrcode/bch15_5.h
-
-jni/zbar/qrcode/bch15_5.h:
